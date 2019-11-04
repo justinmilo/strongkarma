e81ce7f4fbb9729586721247ba46a4d100501e63
justinmilo <justinmilo@me.com>
2019-11-04 12:06:22 -0600

    update EditEntry

--- Strong Karma/Views/EditEntryView.swift
+++ Strong Karma/Views/EditEntryView.swift
@@ -8,73 +8,60 @@
 
 import SwiftUI
 
-struct DetailState {
-    
-    var title : String
-}
 
 struct EditEntryView: View {
-    
-    var meditation : Meditation
-    @ObservedObject var store: OldStore<UserData, AppAction>
-
   
-    @State var value : String = ""
-    
-    @State var text : String = "Test example" {
-        didSet {
-            
-        }
-    }
-    
-    
-    let roundedness : CGFloat = 20
-    
-    var background : some View =  RoundedRectangle(cornerRadius: CGFloat(20)) .foregroundColor(.white)
-    
-    var body: some View {
-        
-        let binding = Binding<String>(get: {
-            self.meditation.entry ?? "No Title"
-               }, set: {
-                   print("text now: \(String(describing: $0))")
-                self.text = $0
-                var med = self.meditation
-                   med.entry = $0
-                self.store.send( .updateMeditation(med) )
-               })
-        
-        
-        
-        return ZStack {
-            AngularGradient(gradient: Gradient(colors: [Color.red, Color.blue]), center: .center).edgesIgnoringSafeArea(.all)
-            VStack{
-                Text(meditation.entry ?? "No Title")
-                    .font(.largeTitle)
-                TextField(meditation.entry ?? "No Title" , text: binding)
-                    .padding(.all)
-                    .background(background)
-                    .padding(EdgeInsets(top: 0, leading: 25, bottom: 5, trailing: 25))
-                TextView(
-                    text: binding
-                )
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                    .padding(.all)
-                    .background(background)
-                    .padding(EdgeInsets(top: 5, leading: 25, bottom: 0, trailing: 25))
-                                
-                
+  var meditation : Meditation
+  @ObservedObject var store: OldStore<UserData, AppAction>
+  
+  @State var title : String
+  @State var entry : String
+  
+  init (meditation med: Meditation, store: OldStore<UserData, AppAction>) {
+    _title = State(initialValue: med.title)
+    _entry = State(initialValue: med.entry)
 
-                    
-                        
-                
-            }
-        }
+    self.meditation = med
+    self.store = store
+  }
+  
+  
+  var body: some View {
+    
+    
+    
+    return      VStack{
+      Text(title)
+        .font(.largeTitle)
+      TextField("Title", text: self.$title, onEditingChanged: {_ in }, onCommit:updateOnCommit )
+      
+      TextView(
+        text: self.$entry, onCommit: updateOnCommit
+      )
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .padding(.all)
+        .padding(EdgeInsets(top: 5, leading: 25, bottom: 0, trailing: 25))
+    }.onDisappear() {
+      self.updateOnCommit()
     }
+    
+  }
+  func updateOnCommit() {
+
+      
+      var med = self.meditation
+      med.title = self.title
+      med.entry = self.entry
+      print("Action should happen")
+      self.store.send(  .replaceOrAddMeditation(med) )
+    
+  }
 }
 
+
+
 struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-      EditEntryView( meditation: Meditation.dummy, store: OldStore<UserData, AppAction>.dummy)
-    }
+  static var previews: some View {
+    EditEntryView( meditation: Meditation.dummy, store: OldStore<UserData, AppAction>.dummy)
+  }
 }
