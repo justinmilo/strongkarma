b69ad8f5dd302ab520940dc45967551eb290822f
Justin Smith <justinmilo@me.com>
2022-01-02 00:13:04 -0500

    Updating with new notification dependencies

--- Strong Karma/Views/AppReducer.swift
+++ Strong Karma/Views/AppReducer.swift
@@ -25,35 +25,6 @@ extension IdentifiedArray where Element == Meditation, ID == UUID {
   }
 }
 
-func formatTime (time: Double) -> String? {
-  let formatter = DateComponentsFormatter()
-  formatter.unitsStyle = .positional // Use the appropriate positioning for the current locale
-  formatter.allowedUnits = [ .hour, .minute, .second ] // Units to display in the formatted string
-  formatter.zeroFormattingBehavior = [ .pad ] // Pad with zeroes where appropriate for the locale
-
-  return  formatter.string(from: time)
-}
-
-
-func date(from string: String) -> Date? {
-  let dateFormatter = DateFormatter()
-  dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
-  dateFormatter.locale = Locale(identifier: "en_US_POSIX") // set locale to reliable US_POSIX
-  //let date = dateFormatter.date(from:isoDate)!
-  
-  
- // let dateFormatter = ISO8601DateFormatter()
-  let date = dateFormatter.date(from:string)
-  return date
-}
-
-func formattedDate(from date:Date) -> String {
-  let dateFormatterPrint = DateFormatter()
-  dateFormatterPrint.dateFormat = "EEEE, MMMM d, yyyy"
-  return dateFormatterPrint.string(from: date)
-}
-
-
 /*
  
  Wednesday, Sep 12, 2018           --> EEEE, MMM d, yyyy
