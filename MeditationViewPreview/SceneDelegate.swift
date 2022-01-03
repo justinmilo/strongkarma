4ba08de622c8730cbe8ce6e5b3801ff70d62319e
Justin Smith <justinmilo@me.com>
2022-01-03 08:16:53 -0500

    notifications are almost working

--- MeditationViewPreview/SceneDelegate.swift
+++ MeditationViewPreview/SceneDelegate.swift
@@ -25,19 +25,6 @@ private let store = Store(initialState: MediationViewState(),
                             uuid: UUID.init)
 )
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-
-    if let windowScene = scene as? UIWindowScene {
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UIHostingController(rootView: MeditationView(store: store))
-        self.window = window
-        window.makeKeyAndVisible()
-    }
-  }
-}
-
 
 @main
 struct FSApp: App {
