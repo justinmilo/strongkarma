e7974b9354e8395ce22b7acf2679e6b4246955cb
Justin Smith <justin@heini.build>
2022-01-12 18:53:57 -0500

    get rid of separate app reducer file and merge into scene delegate

--- Strong Karma/SceneDelegate.swift
+++ Strong Karma/SceneDelegate.swift
@@ -9,7 +9,65 @@
 import UIKit
 import SwiftUI
 import ListViewFeature
+
 import ComposableArchitecture
+import Foundation
+import Models
+import MeditationViewFeature
+import ComposableUserNotifications
+
+
+struct AppState : Equatable {
+    var listViewState: ListViewState
+}
+
+enum AppAction : Equatable {
+  case listAction(ListAction)
+    case didFinishLaunching(notification: UserNotification?)
+    case userNotification(UserNotificationClient.Action)
+    case requestAuthorizationResponse(Result<Bool, UserNotificationClient.Error>)
+}
+
+struct AppEnvironment {
+    var listEnv: ListEnv
+}
+
+let appReducer = Reducer<AppState, AppAction, AppEnvironment>.combine(
+    listReducer.pullback(state: \.listViewState, action: /AppAction.listAction, environment: \AppEnvironment.listEnv),
+    Reducer{ state, action, environment in
+        
+        switch action {
+        case .listAction(_):
+            return .none
+        case .requestAuthorizationResponse:
+            return .none
+        case .didFinishLaunching(notification: let notification):
+            
+            return .merge(
+                environment.listEnv.medEnv.userNotificationClient
+                    .delegate()
+                    .map(AppAction.userNotification),
+                environment.listEnv.medEnv.userNotificationClient.requestAuthorization([.alert, .badge, .sound])
+                    .catchToEffect()
+                    .map(AppAction.requestAuthorizationResponse)
+            )
+        case let .userNotification(.didReceiveResponse(response, completion)):
+            let notification = UserNotification(userInfo: response.notification.request.content.userInfo())
+            
+            return .fireAndForget(completion)
+        case .userNotification(.willPresentNotification(_, completion: let completion)):
+            return .fireAndForget {
+                completion([.list, .banner, .sound])
+            }
+        case .userNotification(.openSettingsForNotification(_)):
+            return .none
+        }
+    }
+)
+
+
+
+
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
